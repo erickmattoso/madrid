c959ae020e41decef5ee510433908a01ed303e90
Ericson Marques <ericsonmarques@Ericsons-MacBook-Pro.local>
2022-12-06 22:43:09 +0100

    first

--- stream_app.py
+++ stream_app.py
@@ -10,7 +10,6 @@ import math
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
-
 def plot_figure(my_df):
     fig = go.Figure(go.Scattermapbox(
         mode="markers",
@@ -62,20 +61,12 @@ def plot_figure(my_df):
             "style": "carto-positron",
             "zoom": 11.8})
     return fig
-
-# functions
-# def parseCSV(filename):
-#     places = list(csv.DictReader(open(filename, newline="")))
-#     print(places)
-#     return places
-
-
+    
 def extractCoords(input):
     coords = []
     for item in input:
         coords.append((float(item["lat"]), float(item["lng"])))
     return coords
-
 
 def compute_euclidean_distance_matrix(locations):
     size = len(locations)
@@ -91,7 +82,6 @@ def compute_euclidean_distance_matrix(locations):
                 distances[from_counter][to_counter] = int(math.hypot(x_d, y_d))
     return distances
 
-
 def create_data_model(csvfile):
     places =  csvfile.to_dict('records') #parseCSV(csvfile)
     coords = extractCoords(places)
@@ -102,7 +92,6 @@ def create_data_model(csvfile):
     data["num_vehicles"] = 1
     data["depot"] = 0
     return data
-
 
 def print_solution_gmaps(manager, routing, assignment, data, val=[]):
     index = routing.Start(0)
@@ -116,23 +105,12 @@ def print_solution_gmaps(manager, routing, assignment, data, val=[]):
         val.append([list_lat, list_long, list_title,list_status])
     return val
 
-
 def distance_callback(from_index, to_index):
     from_node = manager.IndexToNode(from_index)
     to_node = manager.IndexToNode(to_index)
     return data_model["distance_matrix"][from_node][to_node]
 
-
-
-
-
-
-
-
-
-
 st.title('Trip to Madrid')
-
 loc_button = Button(label="Get Location")
 loc_button.js_on_event("button_click", CustomJS(code="""
     navigator.geolocation.getCurrentPosition(
@@ -148,25 +126,23 @@ result = streamlit_bokeh_events(
     override_height=40,
     debounce_time=0)
 
-
-
-
-
-
 df_original = pd.read_csv('csv_csv.csv', index_col=[0])
-fill = st.selectbox("Filter day", options=df_original.columns[4:])
+
+custom_col_0 = st.columns((1, 1))
+
+fill = custom_col_0[0].selectbox("Filter day", options=df_original.columns[4:])
 df_entrada = df_original[df_original[fill].notna()].sort_values(fill)
-
-
-
 my_expander = st.expander(label='Advanced Filters')
-
 with my_expander:
     yyyyy = st.multiselect("Filter Places",options=list(df_entrada['title']),default=list(df_entrada['title']))
+    itovisit = st.checkbox("To Visit")
+    if itovisit:
+        done = df_entrada[df_entrada["status"]!="Done"]
+        yyyyy = list(done['title'])
     df_entrada=df_entrada[df_entrada["title"].isin(yyyyy)]
-    lay = st.columns((.5, 1, .5))
-
-    if lay[0].button("calculate"):
+    custom_col_1 = st.columns((.5, 1, .5))
+    custom_col_1[0].write("Calculate route")
+    if custom_col_1[0].button("calculate"):
         data_model = create_data_model(df_entrada)
         manager = pywrapcp.RoutingIndexManager(len(data_model["distance_matrix"]), data_model["num_vehicles"], data_model["depot"])
         routing = pywrapcp.RoutingModel(manager)
@@ -180,50 +156,25 @@ with my_expander:
         val.append(val[0])
         df_entrada = pd.DataFrame(val)
         df_entrada.columns ="lat","lng","title","status"
-        # df_entrada["lat"] = df_entrada["lat"].astype(float)
-        # df_entrada["lng"] = df_entrada["lng"].astype(float)
-        # st.write(df_entrada)
         st.session_state.df_entrada = df_entrada
 
-
-   
-    column = lay[1].text_input("here")
-
-    if lay[2].button("Save As"):
-
+    column = custom_col_1[1].text_input("Name of Route")
+    custom_col_1[2].write("Click to Save Route")
+    if custom_col_1[2].button("Save As"):
         df_entrada = st.session_state.df_entrada.reset_index()
         new = df_entrada[["title","index"]]
         new.columns = "title",column
         df_original = pd.merge(df_original,new,on="title",how="left")
         df_original.to_csv("csv_csv.csv", index=[0])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 fig = plot_figure(df_entrada)
 st.plotly_chart(fig, use_container_width=True)
-
-cols = st.columns((2, 1, .5))
+custom_col_2 = st.columns((2, 1, .5))
 mylist = df_entrada['title']+" (" + df_entrada['status']+") "
-place = cols[0].selectbox("Status", options=list(mylist))[:-8]
-sts = cols[1].selectbox("Status", options=("Done", "ToDo"))
-if cols[2].button("Update"):
+place = custom_col_2[0].selectbox("Status", options=list(mylist))[:-8]
+sts = custom_col_2[1].selectbox("Status", options=("Done", "ToDo"))
+if custom_col_2[2].button("Update"):
     df_entrada.loc[df_entrada["title"].str.contains(place), "status"] = sts    
     df_entrada.to_csv('csv_csv.csv', index=[0])
-    cols[2].markdown("Saved!")
+    custom_col_2[2].markdown("Saved!")
     st.experimental_rerun()
-
