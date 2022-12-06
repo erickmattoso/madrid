6fbb2fdca56cc124ea057694d0fe7a7493a534a2
Ericson Marques <ericsonmarques@Ericsons-MacBook-Pro.local>
2022-12-06 21:13:18 +0100

    first

--- stream_app.py
+++ stream_app.py
@@ -10,7 +10,6 @@ import math
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
-
 def plot_figure(my_df):
     fig = go.Figure(go.Scattermapbox(
         mode="markers",
@@ -63,19 +62,11 @@ def plot_figure(my_df):
             "zoom": 11.8})
     return fig
 
-# functions
-# def parseCSV(filename):
-#     places = list(csv.DictReader(open(filename, newline="")))
-#     print(places)
-#     return places
-
-
 def extractCoords(input):
     coords = []
     for item in input:
         coords.append((float(item["lat"]), float(item["lng"])))
     return coords
-
 
 def compute_euclidean_distance_matrix(locations):
     size = len(locations)
@@ -91,9 +82,8 @@ def compute_euclidean_distance_matrix(locations):
                 distances[from_counter][to_counter] = int(math.hypot(x_d, y_d))
     return distances
 
-
 def create_data_model(csvfile):
-    places =  csvfile.to_dict('records') #parseCSV(csvfile)
+    places =  csvfile.to_dict('records')
     coords = extractCoords(places)
     distancem = compute_euclidean_distance_matrix(coords)
     data = {}
@@ -102,7 +92,6 @@ def create_data_model(csvfile):
     data["num_vehicles"] = 1
     data["depot"] = 0
     return data
-
 
 def print_solution_gmaps(manager, routing, assignment, data, val=[]):
     index = routing.Start(0)
@@ -115,7 +104,6 @@ def print_solution_gmaps(manager, routing, assignment, data, val=[]):
         list_status = data["places"][manager.IndexToNode(index)]["status"]
         val.append([list_lat, list_long, list_title,list_status])
     return val
-
 
 def distance_callback(from_index, to_index):
     from_node = manager.IndexToNode(from_index)
@@ -132,7 +120,6 @@ def distance_callback(from_index, to_index):
 
 
 st.title('Trip to Madrid')
-
 loc_button = Button(label="Get Location")
 loc_button.js_on_event("button_click", CustomJS(code="""
     navigator.geolocation.getCurrentPosition(
@@ -160,7 +147,6 @@ df_entrada = df_original[df_original[fill].notna()].sort_values(fill)
 
 
 my_expander = st.expander(label='Advanced Filters')
-
 with my_expander:
     yyyyy = st.multiselect("Filter Places",options=list(df_entrada['title']),default=list(df_entrada['title']))
     df_entrada=df_entrada[df_entrada["title"].isin(yyyyy)]
@@ -179,20 +165,14 @@ with my_expander:
         val = print_solution_gmaps(manager, routing, assignment, data_model)
         val.append(val[0])
         df_entrada = pd.DataFrame(val)
-        df_entrada.columns ="lat","lng","title","status"
-        # df_entrada["lat"] = df_entrada["lat"].astype(float)
-        # df_entrada["lng"] = df_entrada["lng"].astype(float)
-        # st.write(df_entrada)
-
-   
+        df_entrada.columns ="lat","lng","title","status"  
     column = lay[1].text_input("here")
 
     if lay[2].button("Save As"):
         # st.write(df_entrada.loc[df_entrada["title"].isin(yyyyy)].index)
-        df_original.loc[df_original["title"].isin(yyyyy), column] =  df_entrada.index
-        
+        # df_original.loc[df_original["title"].isin(yyyyy), column] =  df_entrada.index
         # st.write(df_original)
-        df_original.to_csv("csv_csv.csv", index=[0])
+        # df_original.to_csv("csv_csv.csv", index=[0])
 
 
 
@@ -218,5 +198,5 @@ place = cols[0].selectbox("Status", options=list(mylist))[:-8]
 sts = cols[1].selectbox("Status", options=("Done", "ToDo"))
 if cols[2].button("Update"):
     df_entrada.loc[df_entrada["title"].str.contains(place), "status"] = sts    
-    df_entrada.to_csv('df_entrada.csv', index=[0])
+    df_entrada.to_csv('csv_csv.csv', index=[0])
     cols[2].markdown("Saved!")
